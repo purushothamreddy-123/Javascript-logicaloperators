2361947e32a5405c3e984c71ece33fd8033d461d
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-16 17:43:17 +0530

    Javascript logicaloperators

---
+++ logicaloperators.js
@@ -0,0 +1,23 @@
+const a = true, b = false;
+const c = 4;
+
+// logical AND
+console.log(a && a); 
+console.log(a && b);  
+
+console.log((c > 2) && (c < 2));
+
+
+
+
+// // logical OR
+// const a = true, b = false, c = 4;
+// console.log(a || b); 
+// console.log(b || b); 
+// console.log((c>2) || (c<2)); 
+
+// const a = true, b = false;
+
+// // logical NOT
+// console.log(!a); 
+// console.log(!b);
